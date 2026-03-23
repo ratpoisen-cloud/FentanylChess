677dba4a17c042a2c7fff3d9de1edee623a64cda
ratpoisen-cloud <ratpoisen@gmail.com>
2026-03-23 22:31:20 +0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,13 +1,14 @@
 // Firebase конфигурация
 // Создайте свой проект в Firebase Console и замените эти значения
-const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyDummyKeyForDemoReplaceWithYourOwn",
-    authDomain: "chess-online-demo.firebaseapp.com",
-    databaseURL: "https://chess-online-demo-default-rtdb.firebaseio.com",
-    projectId: "chess-online-demo",
-    storageBucket: "chess-online-demo.appspot.com",
-    messagingSenderId: "123456789012",
-    appId: "1:123456789012:web:abcdef123456"
+const firebaseConfig = {
+  apiKey: "AIzaSyB_7Eh0wuaNY3eb-42uisssvOrSb6ESi_E",
+  authDomain: "fentanylchess.firebaseapp.com",
+  databaseURL: "https://fentanylchess-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "fentanylchess",
+  storageBucket: "fentanylchess.firebasestorage.app",
+  messagingSenderId: "578661463625",
+  appId: "1:578661463625:web:2877feb0d1a38f4961b198",
+  measurementId: "G-N4HCS2P63T"
 };
 
 let firebaseApp = null;
