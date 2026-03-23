c1c6ed4cda6ac566a46e1b06a517071bb1dbf4b6
ratpoisen-cloud <ratpoisen@gmail.com>
2026-03-24 00:10:29 +0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -15,4 +15,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
-export const auth = getAuth(app); // Экспортируем auth для быстрого доступа
+export const auth = getAuth(app);
